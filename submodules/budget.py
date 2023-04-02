8ff2b438126feba63d51ab36c153638a5c7ed61a
anesu <anesukafesu@gmail.com>
2023-04-02 07:05:23 +0200

    recovered deleted submodules

---
+++ submodules/budget.py
@@ -0,0 +1,22 @@
+def budget():
+    sources = ["Taxes", "Loans", "Donations", "Other Sources"]
+    amounts = []
+    for i in range(len(sources)):
+        amount = float(input(f"Enter the amount of {sources[i]}: "))
+        amounts.append(amount)
+
+    total_income = sum(amounts)
+
+    expenses = ["Rent", "Utilities", "Salaries", "Supplies", "Other Expenses"]
+    expense_amounts = []
+    for i in range(len(expenses)):
+        amount = float(input(f"Enter the amount of {expenses[i]}: "))
+        expense_amounts.append(amount)
+
+    total_expenses = sum(expense_amounts)
+
+    annual_budget = total_income - total_expenses
+
+    print(f"Total Income: {total_income}")
+    print(f"Total Expenses: {total_expenses}")
+    return annual_budget
