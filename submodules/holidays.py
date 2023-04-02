df69645cf961c61a7ca3c0a17f5943934bfd565d
anesu <anesukafesu@gmail.com>
2023-04-02 12:08:11 +0200

    finished holidays.py

--- submodules/holidays.py
+++ submodules/holidays.py
@@ -1,28 +1,15 @@
 #!/usr/bin/python3
 from database import create_cursor
 
-holidays_array = [
-    ["Jan 01", "New Year's Day"],
-    ["Feb 01", "Heroe's Day"],
-    ["Apr 07", "Good Friday"],
-    ["Apr 07", "Genocide against the Tutsi Memorial Day"],
-    ["Apr 10", "Easter Monday"],
-    ["Apr 22", "Eid Ul Fitr"],
-    ["May 01", "Worker's Day"],
-    ["Jun 29", "Eid El Haj"],
-    ["Jul 01", "Independence Day"],
-    ["Jul 04", "Liberation Day"],
-    ["Aug 04", "Assumption Day"],
-    ["Dec 25", "Christmas Day"],
-    ["Dec 26", "Boxing Day"]
-]
-
-cursor = create_cursor()
-
-cursor('CREATE TABLE IF NOT EXISTS holidays (date TEXT, name TEXT)')
-
-
 def holidays():
+    # Create a cursor and db connection
+    cursor, db = create_cursor()
+
+    # Get all holidays from the holidays table in the database
+    cursor.execute('SELECT * FROM holidays')
+    
+    # Print the headings
     print("Date   | Holiday Name")
-    for holiday in holidays_array:
-        print(holiday[0], "|", holiday[1])
+
+    for date, holiday in cursor:
+        print(date, "|", holiday)
