39a0d962343abb591c71f3f71acca622f166987c
jmukamani <j.mukamani@alustudent.com>
2023-04-04 01:36:31 +0200

    Projects

--- submodules/projects.py
+++ submodules/projects.py
@@ -11,7 +11,35 @@ duration_months = "12"
 estimated_completion_date = "28 March 2024"
 project_status = "Ongoing"
 
-cursor.execute("INSERT INTO projects(name, location, description, estimated_completion_date, status) VALUES(%s,%s,%s,%s,%s)",(project_name, project_location, project_description, estimated_completion_date, project_status))
+project_name = "Upgrading of unpaved roads to paved"
+project_location = "Musanze, Eastern Province"
+project_description = "Upgrade the roads to meet the international statandards of roads."
+duration_months = "8"
+estimated_completion_date = "30th October 2023"
+project_status = "To be started"
+
+project_name = "Informal settlement upgrade"
+project_location = "Nyarugege, Kigali Province"
+project_description = "Upgrade slums to achieve good sustainable housing in the city of Kigali."
+duration_months = "12"
+estimated_completion_date = "28 April 2025"
+project_status = "Ongoing"
+
+project_name = "Install and operationalize weighbridge stations"
+project_location = "All the Rwandan Borders"
+project_description = "The weighbridge will facilitate easy, smooth and reliable movement at all the border post."
+duration_months = "6"
+estimated_completion_date = "20 september 2023"
+project_status = "Ongoing"
+
+project_name = "Nyabarongo hydro power project"
+project_location = "Rukira, Eastern Province"
+project_description = "The poject is for Rwanda to generate its own hydro electric power to supply electrity at low cost to the citizens of Rwanda."
+duration_months = "18"
+estimated_completion_date = "20 september 2025"
+project_status = "Ongoing"
+
+cursor.execute("INSERT INTO projects(name,location, description, estimated_completion_date, status) VALUES(%s,%s,%s,%s,%s)",(project_name, project_location, project_description, estimated_completion_date, project_status))
 
 db.commit()
 
