099cced9f9ebf7e96d1dc200d52fdef57dbab659
anesu <anesukafesu@gmail.com>
2023-04-05 03:51:10 +0200

    Revert "worked on menu"
    This reverts commit 6f57e4658abed0231645b512f70227e7592b68a3.

--- main.py
+++ main.py
@@ -61,7 +61,6 @@ while True:
         break
 
     def trigger_exit():
-        global exit_program
         exit_program = True
 
     def show_help_info():
@@ -105,4 +104,4 @@ while True:
         trigger_exit()
 
     # Else the user opted to return to main menu
-    # So we make sure exit_program is set to false
+    # In which case we just let the program run
