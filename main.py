786330181d04ca488cac69ace99a90b1a9d76473
anesu <anesukafesu@gmail.com>
2023-04-05 03:36:31 +0200

    created a menu

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3
-import sys
-from submodules import budget
-from submodules import holidays
-from submodules import database
+from simple_term_menu import TerminalMenu
+from submodules.holidays import holidays
+from submodules.projects import projects
+from submodules.economy import economy
+from submodules.population import population
+from submodules.tenders import tenders
+from submodules.contact import contacts
+from os import system
 
+welcome_screen = """
+███╗   ███╗██╗███████╗      ██╗███╗   ██╗███████╗ ██████╗ ██████╗ ███╗   ███╗
+████╗ ████║██║██╔════╝      ██║████╗  ██║██╔════╝██╔═══██╗██╔══██╗████╗ ████║
+██╔████╔██║██║███████╗█████╗██║██╔██╗ ██║█████╗  ██║   ██║██████╔╝██╔████╔██║
+██║╚██╔╝██║██║╚════██║╚════╝██║██║╚██╗██║██╔══╝  ██║   ██║██╔══██╗██║╚██╔╝██║
+██║ ╚═╝ ██║██║███████║      ██║██║ ╚████║██║     ╚██████╔╝██║  ██║██║ ╚═╝ ██║
+╚═╝     ╚═╝╚═╝╚══════╝      ╚═╝╚═╝  ╚═══╝╚═╝      ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝
+"""
 
-def help():
-    # Code to display help information goes here
-    print("""
-Options:
+version = "(Version 1.0.0)"
+
+product_intro = """
+Mis-inform aims to bridge the information gap and keep citizens engaged in civic matters.
+We do this by providing up-to-date information on government affairs.
+"""
+
+guide = """
+Here are the current features:
+
     press      Display the latest press releases issued by the government.
     budget     Display the actual and projected income and expenses of the government.
     projects   Display information about all ongoing government projects, including their completion rates and estimated completion dates.
@@ -17,50 +35,73 @@ Options:
     holidays   Display the list of public holidays for the current year.
     tenders    Display all pending tender notices.
     contact    Display contact information for government departments.
-    -h, --help Display the help page and exit.
-""")
+    help       Display this informaiton page again
+    exit       Exit MIS-Inform
+"""
+
+def show_terminal_menu(options = [], menu_title=""):
+    centered_options = map(lambda option: option.center(77), options)
+    terminal_menu = TerminalMenu(centered_options, menu_cursor="", title=menu_title)
+    terminal_menu_selection_index = terminal_menu.show()
+
+    # Get the selected option by name
+    # Using names is more readable and generally safer than indexes
+    return options[terminal_menu_selection_index]
 
 
-# print(budget.Budget1.get_allocations())
 
-# Main program loop
+exit_message = "01000111 01101111 01101111 01100100  01100010 01111001 01100101"
+
+exit_program = False
+
+# Loop to render screen
 while True:
+    if exit_program:
+        print(exit_message)
+        break
 
-    print("Welcome to Misinform! Please select an option:")
-    print("1. Press")
-    print("2. Budget")
-    print("3. Projects")
-    print("4. Economy")
-    print("5. Population")
-    print("6. Holidays")
-    print("7. Tenders")
-    print("8. Contact")
-    print("9. Help")
-    print("0. Exit")
-    choice = input("Enter your choice: ")
+    def trigger_exit():
+        exit_program = True
 
-    if choice == "1":
-        press()
-    elif choice == "2":
-        print(budget.get_allocations(2020))
-        print(budget.get_budget(2019))
-        print(budget.get_revenue(2014))
-    elif choice == "3":
-        projects()
-    elif choice == "4":
-        economy()
-    elif choice == "5":
-        population()
-    elif choice == "6":
-        holidays.holidays()
-    elif choice == "7":
-        tenders()
-    elif choice == "8":
-        contact()
-    elif choice == "9":
-        help()
-    elif choice == "0":
-        print("Thank you for using Misinform!")
-        sys.exit(0)
-    else:
-        print("Invalid choice. Please try again.")
+    def show_help_info():
+        print(guide)
+
+    # Print welcome screen
+    system("clear")
+    print(welcome_screen)
+    print(version)
+    
+    # Options
+    options = {
+        'Press': None,
+        'Budget': None,
+        'Projects': projects,
+        'Economy': economy,
+        'Population': population,
+        'Holidays': holidays,
+        'Tenders': tenders,
+        'Contact': contacts,
+        'Help': show_help_info,
+        'Exit': trigger_exit
+    }
+
+    options_list = list(options.keys())
+    s = show_terminal_menu(options_list, "Main Menu")
+
+    # Calling the function for the option the user picked
+    options[s].__call__()
+    
+
+    # Create an exit menu
+    print("")
+    print("")
+    print("")
+    exit_options = ["Back to Main Menu", "Exit"]
+    
+    s = show_terminal_menu(exit_options, "Exit options")
+
+    if s == "Exit":
+        trigger_exit()
+
+    # Else the user opted to return to main menu
+    # In which case we just let the program run
